b811d6a63c8107df0c1862e6d27bcc4f04df45a3
nasan <nesrin.asan@hepsiburada.com>
2025-12-15 22:59:50 +0300

    #000 mcp tool

--- src/main/java/com/backendguru/ChatbotService/controller/ChatbotController.java
+++ src/main/java/com/backendguru/ChatbotService/controller/ChatbotController.java
@@ -14,6 +14,11 @@ public class ChatbotController {
         this.chatService = chatService;
     }
 
+    /** 1
+     * iade işlemlerini başlat.
+     * @param chatbotRequestDto
+     * @return
+     */
     @PostMapping("/orderReturnProcess")
     public String orderReturnProcess(@RequestBody ChatbotRequestDto chatbotRequestDto){
         String s = chatService.orderReturnProcess(chatbotRequestDto.prompt());
@@ -22,10 +27,53 @@ public class ChatbotController {
         return s;
     }
 
+    /** 2
+     * return true case
+     * @param chatbotRequestDto
+     * @return
+     */
     @PostMapping("/v2/orderReturnProcess")
     public ReturnOrderProcessResponseDto orderReturnProcessReturnTrue(@RequestBody ChatbotRequestDto chatbotRequestDto){
         return chatService.orderReturnProcessReturnTrue(chatbotRequestDto.prompt());
 
     }
+
+    /** 3
+     * orderReturnProcessReturnTrueWithoutConverter
+     * return
+     * @param chatbotRequestDto
+     * @return
+     */
+    @PostMapping("/v3/orderReturnProcess")
+    public ReturnOrderProcessResponseDto orderReturnProcessReturnTrueWithoutConverter(@RequestBody ChatbotRequestDto chatbotRequestDto){
+        return chatService.orderReturnProcessReturnTrueWithoutConverter(chatbotRequestDto.prompt());
+
+    }
+
+    /**
+     * 4
+     * @param chatbotRequestDto
+     * @return
+     */
+    @PostMapping("/v4/orderReturnProcess")
+    public String orderReturnProcessReturnWrongPrompt(@RequestBody ChatbotRequestDto chatbotRequestDto){
+        return chatService.orderReturnProcessReturnWrongPrompt(chatbotRequestDto.prompt());
+
+    }
+
+    /** 5
+     * iade işlemlerini başlat. Mail gönder
+     * @param chatbotRequestDto
+     * @return
+     */
+    @PostMapping("/orderReturnProcessSendMail")
+    public String orderReturnProcessSendMail(@RequestBody ChatbotRequestDto chatbotRequestDto){
+        String s = chatService.orderReturnProcessSendMail(chatbotRequestDto.prompt());
+
+        System.out.println("test");
+        return s;
+    }
+
+
 
 }
--- src/main/java/com/backendguru/ChatbotService/service/ChatService.java
+++ src/main/java/com/backendguru/ChatbotService/service/ChatService.java
@@ -29,8 +29,8 @@ public class ChatService {
 
     public String orderReturnProcess(String prompt) {
         String system = """
-                Görevin: mevcut MCP araçlarını kullanarak kullanıcının talebi olan siparişi iade etme sürecini başlatan akışı oluştur.  
-                
+                Görevin: mevcut MCP araçlarını kullanarak kullanıcının talebi olan siparişi iade etme sürecini başlatan akışı oluştur
+                {format}
                 """;
 
         String user = "Kullanıcının iade etmek istediği sipariş detayı:" + prompt;
@@ -45,6 +45,7 @@ public class ChatService {
     }
 
 
+    // Tool KULLANMADAN LLM'den Structured Output İstiyorsan BeanOutputConverter kullanılabilir.
     public ReturnOrderProcessResponseDto orderReturnProcessReturnTrue(String promptStr) {
 
         BeanOutputConverter<ReturnOrderProcessResponseDto> beanOutputConverter = new BeanOutputConverter<>(new ParameterizedTypeReference<ReturnOrderProcessResponseDto>() { });
@@ -52,21 +53,87 @@ public class ChatService {
         String format = beanOutputConverter.getFormat();
 
         String system = """
+                Görevin: mevcut MCP araçlarını kullanarak kullanıcının talebi olan siparişi iade etme sürecini başlatan akışı oluştur.
+                 
+                {format}
+                """;
+
+        String user = "Kullanıcının iade etmek istediği sipariş detayı:" + promptStr;
+        PromptTemplate promptTemplate = PromptTemplate.builder().template(system + user).build();
+        Prompt promptV = promptTemplate.create(Map.of("format", format));
+
+        Generation generation = chatModel.call(promptV).getResult();
+
+        return beanOutputConverter.convert(generation.getOutput().getText());
+
+    }
+
+    public ReturnOrderProcessResponseDto orderReturnProcessReturnTrueWithoutConverter(String promptStr) {
+        String system = """
+            Görevin: mevcut MCP araçlarını kullanarak kullanıcının talebi olan siparişi iade etme sürecini başlatan akışı oluştur.
+            """;
+
+        String user = "Kullanıcının iade etmek istediği sipariş detayı:" + promptStr;
+
+        return chatClient.prompt()
+                .system(system)
+                .user(user)
+                .call()
+                .entity(ReturnOrderProcessResponseDto.class);
+    }
+
+    public String orderReturnProcessReturnWrongPrompt(String prompt) {
+        String system = """
+                 Sen bir e-ticaret sipariş yönetim asistanısın.
+                         
+                            YAPTIĞIN İŞLER:
+                            - Sipariş iade işlemleri
+                            - Sipariş iptal işlemleri
+                            - Sipariş durum sorgulama
+                            
+                              YAPMADIĞIN İŞLER:
+                             - Yeni ürün satışı
+                             - Uçak/otobüs bileti işlemleri
+                             - Otel rezervasyonu
+                             - Sipariş dışı hizmetler
+                            \s
+                             Eğer kullanıcı kapsam dışı bir istek yaparsa:
+                             1. Kibarca reddet
+                             2. Senin yapabildiğin işleri söyle
+                             3. Alternatif öner (varsa)
+                            \s
+                             Mevcut MCP araçlarını kullanarak görevini yerine getir.
+                """;
+
+        String user = "Kullanıcı isteği:" + prompt;
+
+        String content = chatClient.prompt()
+                .system(system)
+                .user(user)
+                .call()
+                .content();
+        System.out.println("sonuç alındı");
+        return content;
+    }
+
+
+
+    public String orderReturnProcessSendMail(String prompt) {
+        String system = """
                 Görevin: mevcut MCP araçlarını kullanarak kullanıcının talebi olan siparişi iade etme sürecini başlatan akışı oluştur. ve kullanıcya
                  hepsiburada@gmail.com adresinden siparişin durumu ile ilgili 
                 anlamlı bir mail gönder. 
                 {format}
                 """;
 
-        String user = "Kullanıcı maili nesrin.asan@gmail.com. Kullanıcının iade etmek istediği sipariş detayı:" + promptStr;
-        PromptTemplate promptTemplate = PromptTemplate.builder().template(system + user).build();
-        Prompt promptV = promptTemplate.create(Map.of("format", format));
+        String user = "Kullanıcı maili nesrin.asan@gmail.com. Kullanıcının iade etmek istediği sipariş detayı:" + prompt;
 
-        // 3. LLM çağrısı
-        Generation generation = chatModel.call(promptV).getResult();
-
-        // 4. JSON çıktısını DTO'ya dönüştürme (Sizin ProductDto dönüşümünüze benzer)
-        return beanOutputConverter.convert(generation.getOutput().getText());
-
+        String content = chatClient.prompt()
+                .system(system)
+                .user(user)
+                .call()
+                .content();
+        System.out.println("sonuç alındı");
+        return content;
     }
 }
